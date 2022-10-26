379bde6b14b03b3dc1f448bb1137a73486365c47
Amanda Vien <amandakvien@gmail.com>
2022-10-26 15:55:31 -0700

    edit design on backend route working

--- app/api/design_routes.py
+++ app/api/design_routes.py
@@ -3,6 +3,9 @@ from app.models import Design, Brand, User, db
 
 from flask_login import current_user, login_user, logout_user, login_required
 
+from ..forms.add_design_form import AddDesignForm
+from ..forms.edit_design_form import EditDesignForm
+
 from sqlalchemy import func
 
 design_routes = Blueprint('designs', __name__)
@@ -19,7 +22,7 @@ def validation_errors_to_error_messages(validation_errors):
     return errorMessages
 
 
-#LOAD ALL DESIGNS
+#LOAD ALL DESIGNS ----------------------------------
 @design_routes.route('/')
 def all_designs():
   design = Design.query.all()
@@ -27,7 +30,21 @@ def all_designs():
   return jsonify({ 'Designs': all_des })
 
 
-#LOAD ALL USER'S DESIGNS
+#LOAD SINGLE DESIGN --------------------------------
+@design_routes.route('/<int:design_id>')
+def get_one_design(design_id):
+  design = Design.query.get(design_id)
+  if not design:
+    return jsonify({
+      "message": "Design could not be found.",
+      "status_code": 404
+    }), 404
+
+  des = design.to_dict()
+  return des
+
+
+#LOAD ALL USER'S DESIGNS ---------------------------
 @design_routes.route('/current')
 @login_required
 def get_all_user_designs():
@@ -49,4 +66,92 @@ def get_all_user_designs():
   return 'Current user does not have any designs yet.'
 
 
-#
+#CREATE A DESIGN -------------------------------------
+@design_routes.route('/new', methods=['POST'])
+@login_required
+def add_design():
+  user = current_user.to_dict()
+  user_id = user['id']
+  form = AddDesignForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+
+  # Body validation error handlers:
+  login_val_error = {
+      "message": "Validation error",
+      "status_code": 400,
+      "errors": {}
+  }
+
+  if not form.data['name']:
+      login_val_error["errors"]["name"] = "Name of design is is required."
+  if len(login_val_error["errors"]) > 0:
+      return jsonify(login_val_error), 400
+
+  if form.validate_on_submit():
+      design = Design(
+        user_id=user_id,
+        name=form.data["name"]
+      )
+
+      db.session.add(design)
+      db.session.commit()
+
+      des = design.to_dict()
+      return des
+
+  return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+
+#UPDATE DESIGN --------------------------------------
+@design_routes.route('/<int:design_id>', methods=['PUT'])
+@login_required
+def edit_design(design_id):
+  user = current_user.to_dict()
+  user_id = user['id']
+
+  form = EditDesignForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+
+  design_update = Design.query.get(design_id)
+  if not design_update:
+    return jsonify({
+      "message": "Design could not be found.",
+      "status_code": 404
+    })
+
+  # Body validation error handlers:
+  login_val_error = {
+      "message": "Validation error",
+      "status_code": 400,
+      "errors": {}
+  }
+
+  if not form.data['name']:
+      login_val_error["errors"]["name"] = "Name of design is is required."
+  if len(login_val_error["errors"]) > 0:
+      return jsonify(login_val_error), 400
+
+  # Check if current user owns this design
+  if user_id != design_update.to_dict()['user_id']:
+      return {
+        "message": "Forbidden",
+        "status_code": 403
+      }, 403
+
+  if user_id == design_update.to_dict()['user_id']:
+      if form.validate_on_submit():
+        design_update.user_id = user_id
+        design_update.name = form.data['name']
+
+        db.session.commit()
+
+        des = design_update.to_dict()
+        return des
+
+      else:
+        return {
+          'errors': validation_errors_to_error_messages(form.errors)
+          }, 400
+
+  else:
+        return {"message": "Forbidden", "status_code": 403}, 403
