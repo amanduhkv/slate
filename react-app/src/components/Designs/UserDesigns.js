66873d6c0a5f7bdae837853c048fa1f74e95a26d
Amanda Vien <amandakvien@gmail.com>
2022-10-31 21:09:20 -0700

    styled users recent designs

--- react-app/src/components/Designs/UserDesigns.js
+++ react-app/src/components/Designs/UserDesigns.js
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { getAllUserDesigns, clearData } from "../../store/designs";
 
 export default function UserDesigns() {
   const designs = useSelector(state => state.designs.allDesigns);
   const desArr = Object.values(designs);
   const dispatch = useDispatch();
+  const history = useHistory();
   console.log('DESARR', desArr)
 
   useEffect(() => {
@@ -15,10 +17,10 @@ export default function UserDesigns() {
   }, [dispatch])
 
   return (
-    <div>
+    <div className="recent-des">
       {desArr.map(des => (
-        <div className="des-containers">
-          {des.name}
+        <div className="des-containers" onClick={() => history.push(`/designs/${des.id}`)}>
+          <h3>{des.name}</h3>
         </div>
       ))}
     </div>
